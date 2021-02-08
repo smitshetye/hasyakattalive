5b2c2054a3550e4fe3fc00fde469cbf42ee15635
Smit Bhaskar Shetye <66062619+smitshetye@users.noreply.github.com>
2021-02-08 19:19:31 +0530

    Update connect.php

--- server/connect.php
+++ server/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = 'sql106.epizy.com';
-$database = 'epiz_27694097_live';
-$username = 'epiz_27694097';
-$password = 'c4nyN5xqg5q4';
+$servername = 'ec2-52-22-161-59.compute-1.amazonaws.com';
+$database = 'd5ifgimakiqcre';
+$username = 'zuctybtoaeumqy';
+$password = '2c174edee7f46e0fc78a09fa4de7556887da8aa158ad6f7a5c542bfe14bd17e2';
 
 // Create connection
 
